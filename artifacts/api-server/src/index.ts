2aae04d1882e4996d1f206729c2c6d1507866765
LetsDoittttt <avonbarksdalejr@proton.me>
2026-05-31 23:53:56 +0000

    wire userbot into server

--- artifacts/api-server/src/index.ts
+++ artifacts/api-server/src/index.ts
@@ -1,5 +1,7 @@
 import app from "./app";
 import { logger } from "./lib/logger";
+import { startUserbot } from "./userbot";
+
 
 const rawPort = process.env["PORT"];
 
@@ -22,4 +24,5 @@ app.listen(port, (err) => {
   }
 
   logger.info({ port }, "Server listening");
+  startUserbot().catch(e => logger.error({ err: e }, "Userbot failed"));
 });
