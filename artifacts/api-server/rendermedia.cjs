99b99e5bed9d66aeb0283e9800802eebb2d80d1b
LetsDoittttt <avonbarksdalejr@proton.me>
2026-06-01 15:04:44 +0000

    add rendermedia test script

--- artifacts/api-server/rendermedia.cjs
+++ artifacts/api-server/rendermedia.cjs
@@ -1,30 +1,36 @@
 const { TelegramClient } = require('telegram');
 const { StringSession } = require('telegram/sessions');
+
 (async () => {
   const client = new TelegramClient(
     new StringSession('1AQAOMTQ5LjE1NC4xNzUuNTQBuzolMKzxsfjT49i5beIAopfRaPfknVGcYzPTXIS6F6D4uw0fUk5ISBGxqE/rS4nV5gHm9QlynhQtfoeFO1mXJOYsDFZSDFIaCBHr/ABwTr51QKWmkiymGIeX+fJmyivyZwb+1kxRw3ZbNbE45RAS3rJ1yM/V76g9qJsob/jxL41RJBigHedWSt3X7eT8A1jl9h86Hefi2yRv/Op6vHHbflkpLVCvW2L6OATYNhN8CaTOKBchcUec5jea+vT0QNJejkV8HHTlCh6oJgE2IVNZECh/suqZhZISsT8F6IIfhtpn9W7EssT7cI8DCydc28UJLzLsAZp8O0HgczLZe4GsHek='),
     37071709, 'c6a0b305d4b5eae76f01103133acbd8d', { connectionRetries: 5 }
   );
+
   await client.connect();
-  const msgs = await client.getMessages(-1003924753309, { ids: [3217] });
-  const msg = msgs[0];
-  const url = 'https://link-center.net/230558/DLHTzrLjTayx';
-  const res = await fetch('https://attached-bot.onrender.com/api/bypass/test', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ url })
-  });
-  const data = await res.json();
-  console.log('Link result:', data.finalUrl);
-  if (data.success && msg.media) {
-    console.log('Sending media download request to Render...');
-    const res2 = await fetch('https://attached-bot.onrender.com/api/process-media', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ messageId: 3217, channelId: -1003924753309, finalUrl: data.finalUrl })
-    });
-    console.log('Media response:', await res2.text());
+  console.log('Connected!');
+
+  const msgs = await client.getMessages(-1003924753309, { limit: 10 });
+  const msg = msgs.find(m => !!m.media);
+
+  if (!msg) {
+    console.log('No media found in last 10 messages.');
+    await client.disconnect();
+    process.exit(0);
   }
+
+  console.log('Found message ID:', msg.id, '- downloading media...');
+  const buffer = await client.downloadMedia(msg, { workers: 4 });
+
+  if (buffer && buffer.length > 0) {
+    console.log('Downloaded:', buffer.length, 'bytes - sending...');
+    const inputPeer = await client.getInputEntity(-1003924753309);
+    await client.sendFile(inputPeer, { file: buffer, caption: msg.text || '' });
+    console.log('Done! Media sent successfully.');
+  } else {
+    console.log('Failed - buffer was empty.');
+  }
+
   await client.disconnect();
   process.exit(0);
 })();
